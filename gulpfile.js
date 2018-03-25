d6d007cf0213a65768fb23c8f62161299c9b87b1
Johnny_Wu <johnny_wu@Johnny-Wude-MacBook-Pro.local>
2018-03-25 11:46:24 +0800

    week1 css homework2 #1

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('./week1_css/homework5/css/*.scss')
+    gulp.src('./week1_css/homework*/css/*.scss')
         .pipe(sass())
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
-
-    gulp.src('./week1_css/homework3/css/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest(function(f) {
-        return f.base;
-    }))    
 });
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('./week1_css/homework5/css/*.scss', ['sass']);
-    gulp.watch('./week1_css/homework3/css/*.scss', ['sass']);
+    gulp.watch('./week1_css/homework*/css/*.scss', ['sass']);
 })
